1d408068faa356795df5e04fe30e72e753a0eec4
mcarbenay <michael@carbenay.info>
2020-07-07 23:00:33 +0200

    Added empty OVH dns provider

---
+++ src/LetsEncrypt.Azure.Core.V2/DnsProviders/OvhFrDnsProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsEncrypt.Azure.Core.V2.DnsProviders
+{
+    public class OvhFrDnsProvider : IDnsProvider
+    {
+        public int MinimumTtl
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Task Cleanup(string recordSetName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PersistChallenge(string recordSetName, string recordValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
